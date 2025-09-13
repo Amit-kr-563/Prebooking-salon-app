242e921bd7d2a92ab59184ae609513e5bc4630f5
Amit Kumar <kumaramitbxr2004@gmail.com>
2025-09-13 12:11:14 +0530

    Fix UserNavbar import path

--- client/src/pages/UserDashboard.jsx
+++ client/src/pages/UserDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import UserNavbar from "../components/userNavbar";
+import UserNavbar from "../components/UserNavbar";
 import { getCurrentUser } from "../service/api"; 
 
 const UserDashboard = () => {
